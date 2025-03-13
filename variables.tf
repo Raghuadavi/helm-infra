30c89b323b2202341af2f579d334195d8c36e63a
Raghuadavi <126375262+Raghuadavi@users.noreply.github.com>
2025-03-13 17:04:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "project_id" {
 variable "region" {
   description = "The region where the resources will be created"
   type        = string
-  default     = "us-south1"  // Change to a region with sufficient quota
+  default     = "asia-south1"  // Change to a region with sufficient quota
 }
 
 variable "cluster_name" {
