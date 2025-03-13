34025a54a20e3ed17a57b09f68d2a0e5688eeff6
Raghuadavi <126375262+Raghuadavi@users.noreply.github.com>
2025-03-13 18:15:41 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -4,7 +4,7 @@ data "google_client_config" "default" {}
 data "google_container_cluster" "gke_cluster" {
   name     = var.cluster_name
   location = var.region # Change as per your cluster
-  depends_on = [google_container_cluster.new-cluster]
+  depends_on = [google_container_cluster.newcluster]
 }
 
 # Kubernetes Provider using GKE authentication
@@ -37,5 +37,5 @@ module "delegate" {
   delegate_image = "harness/delegate:25.02.85300"
   replicas = 1
   upgrader_enabled = true
-  depends_on       = [google_container_cluster.new-cluster]
+  depends_on       = [google_container_cluster.newcluster]
 }
